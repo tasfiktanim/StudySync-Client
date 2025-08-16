23eb0b9f7cde2f120058a9a91774463248bc9126
tasfiktanim <tasfiktanimofficial@gmail.com>
2025-08-16 13:38:00 +0600

    Sticky Nav

--- src/Layouts/RootLayouts.jsx
+++ src/Layouts/RootLayouts.jsx
@@ -6,13 +6,17 @@ import Footer from '../Shared/Footer/Footer';
 const RootLayouts = () => {
   return (
     <div className="min-h-screen w-full bg-base-100 flex flex-col">
-      <Navbar />
+        
+      <header className="sticky top-0 z-50 w-full">
+        <Navbar />
+      </header>
+
       <main className="flex-grow">
-      
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
           <Outlet />
         </div>
       </main>
+
       <Footer />
     </div>
   );
